ea001c2020f296210e342b8fec033d5d7b51ff33
samirmammadli <samir4ik86@gmail.com>
2017-05-15 21:52:20 +0400

    1

--- ConsoleApplication1/ConsoleApplication1/Source.cpp
+++ ConsoleApplication1/ConsoleApplication1/Source.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
+#include <Windows.h>
 using namespace std;
 
 void main()
 {
+	HANDLE h = GetStdHandle(STD_OUTPUT_HANDLE);
 	const int step = 15;
 	int arr[step][step] = {};
 	int size = step, count = 1;
@@ -18,8 +20,31 @@ void main()
 					arr[j][k] = count;
 					count++;
 				}
+				else if (k == size - 1 && j < size - 1)
+				{
+					arr[j][k] = count;
+					count++;
+				}
+				else if (j == size - 1)
+				{
+
+					arr[j][step - k - 1] = count;
+					count++;
+				}
 			}
 		}
+		for (int j = i; j < size; j++)
+		{
+			for (int k = i; k < size; k++)
+			{
+				if (k == i  && j < size -1 && j > i)
+				{
+					arr[step -j - 1][k] = count;
+					count++;
+				}
+			}
+		}
+		size = step;
 	}
 
 
@@ -27,10 +52,17 @@ void main()
 	//Display on the screen
 	for (int i = 0; i < step; i++)
 	{
+		SetConsoleTextAttribute(h, i + 1);
 		for (int j = 0; j < step; j++)
 		{
 			cout << arr[i][j] << "\t";
 		}
 		cout << endl;
 	}
+
+	/*for (int i = 0; i < 220; i++)
+	{
+		SetConsoleTextAttribute(h, i);
+		cout << i << "\t";
+	}*/
 }
