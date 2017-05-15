f2f884437276e3ebfad40750dbce388ac63684d4
samirmammadli <samir4ik86@gmail.com>
2017-05-15 20:06:37 +0400

    d

--- ConsoleApplication1/ConsoleApplication1/Source.cpp
+++ ConsoleApplication1/ConsoleApplication1/Source.cpp
@@ -1,1 +1,16 @@
-\\uytuy
+#include <iostream>
+using namespace std;
+
+void main()
+{
+	const int size = 15;
+	int arr[size][size] = {};
+	for (int i = 0; i < size; i++)
+	{
+		for (int j = 0; j < size; j++)
+		{
+			cout << arr[i][j] << "\t";
+		}
+		cout << endl;
+	}
+}
