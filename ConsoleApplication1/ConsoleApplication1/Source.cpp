951ae0bc8fd4b402add793c9c346c4c0c5cc8ebc
Samir <Samir4ik86@gmail.com>
2017-05-16 16:50:24 +0400

    Colors

--- ConsoleApplication1/ConsoleApplication1/Source.cpp
+++ ConsoleApplication1/ConsoleApplication1/Source.cpp
@@ -7,7 +7,7 @@ void main()
 	HANDLE h = GetStdHandle(STD_OUTPUT_HANDLE);
 	const int step = 15;
 	int arr[step][step] = {};
-	int size = step, count = 1;
+	int size = step, count = 1, color = 1;
 	for (int i = 0; i < step; i++)
 	{
 		size -= i;
@@ -27,7 +27,6 @@ void main()
 				}
 				else if (j == size - 1)
 				{
-
 					arr[j][step - k - 1] = count;
 					count++;
 				}
@@ -37,9 +36,9 @@ void main()
 		{
 			for (int k = i; k < size; k++)
 			{
-				if (k == i  && j < size -1 && j > i)
+				if (k == i  && j < size - 1 && j > i)
 				{
-					arr[step -j - 1][k] = count;
+					arr[step - j - 1][k] = count;
 					count++;
 				}
 			}
@@ -52,13 +51,31 @@ void main()
 	//Display on the screen
 	for (int i = 0; i < step; i++)
 	{
-		SetConsoleTextAttribute(h, i + 1);
 		for (int j = 0; j < step; j++)
 		{
+			if (arr[i][j] < 57)
+				SetConsoleTextAttribute(h, color);
+			else if (arr[i][j] < 105)
+				SetConsoleTextAttribute(h, color + 1);
+			else if (arr[i][j] < 145)
+				SetConsoleTextAttribute(h, color + 2);
+			else if (arr[i][j] < 177)
+				SetConsoleTextAttribute(h, color + 3);
+			else if (arr[i][j] < 201)
+				SetConsoleTextAttribute(h, color + 4);
+			else if (arr[i][j] < 217)
+				SetConsoleTextAttribute(h, color + 5);
+			else if (arr[i][j] < 225)
+				SetConsoleTextAttribute(h, color + 6);
+			else
+			{
+				SetConsoleTextAttribute(h, color + 7);
+			}
 			cout << arr[i][j] << "\t";
 		}
 		cout << endl;
 	}
+	SetConsoleTextAttribute(h, color + 7);
 
 	/*for (int i = 0; i < 220; i++)
 	{
