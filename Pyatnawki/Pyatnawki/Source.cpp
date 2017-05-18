16bffde4df94f20193a3ab25af8147048acfbbef
Samir <Samir4ik86@gmail.com>
2017-05-18 17:21:59 +0400

    1

---
+++ Pyatnawki/Pyatnawki/Source.cpp
@@ -0,0 +1,102 @@
+#include <iostream>
+#include <time.h>
+#include <conio.h>
+#include <Windows.h>
+using namespace std;
+
+void main()
+{
+	HANDLE h = GetStdHandle(STD_OUTPUT_HANDLE);
+	srand(time(NULL));
+	const int size = 4;
+	int arr[size][size];
+	int key, zeroX = 0, zeroY = 0;
+	int difficulty, rand1, rand2, rand3, rand4, buffer, count  = 0;
+	cout << "Select difficulty: ";
+	cin >> difficulty;
+	for (int i = 0; i < size; i++)
+	{
+		for (int j = 0; j < size; j++)
+		{
+			arr[i][j] = count;
+			count++;
+		}
+	}
+		//Change random position
+	for (int i = 0; i < difficulty; i++)
+	{
+		rand1 = rand() % size;
+		rand2 = rand() % size;
+		rand3 = rand() % size;
+		rand4 = rand() % size;
+
+
+		buffer = arr[rand1][rand2];
+		arr[rand1][rand2] = arr[rand3][rand4];
+		arr[rand3][rand4] = buffer;
+		if (arr[rand1][rand2] == 0)
+		{
+			zeroX = rand1, zeroY = rand2;
+		}
+		else if (arr[rand3][rand4] == 0)
+		{
+			zeroX = rand3, zeroY = rand4;
+		}
+	}
+	while (true)
+	{
+		//Draw
+		SetConsoleCursorPosition(h, { 0,0 });
+		for (int i = 0; i < size; i++)
+		{
+			for (int j = 0; j < size; j++)
+			{
+				if (arr[i][j] == 0)
+				{
+					SetConsoleTextAttribute(h, 12);
+				}
+				else
+				{
+					SetConsoleTextAttribute(h, 7);
+				}
+				cout << arr[i][j] << "\t";
+			}
+			cout << "\n\n";
+		}
+		     // Moves
+			key = _getch();
+			if (key == 224)
+			{
+				key = _getch();
+				//Left
+				if (key == 75 && zeroY > 0)
+				{
+					arr[zeroX][zeroY] = arr[zeroX][zeroY - 1];
+					zeroY--;
+					arr[zeroX][zeroY] = 0;
+				}
+				//Right
+				else if (key == 77 && zeroY < size - 1)
+				{
+					arr[zeroX][zeroY] = arr[zeroX][zeroY + 1];
+					zeroY++;
+					arr[zeroX][zeroY] = 0;
+				}
+				//Up
+				else if (key == 72 && zeroX > 0)
+				{
+					arr[zeroX][zeroY] = arr[zeroX - 1][zeroY];
+					zeroX--;
+					arr[zeroX][zeroY] = 0;
+				}
+				//Down
+				else if (key == 80 && zeroX < size - 1)
+				{
+					arr[zeroX][zeroY] = arr[zeroX + 1][zeroY];
+					zeroX++;
+					arr[zeroX][zeroY] = 0;
+				}
+			}
+		//system("cls");
+	}
+}
