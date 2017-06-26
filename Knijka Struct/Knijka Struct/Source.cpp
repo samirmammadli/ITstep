dbdfaff341ad115e51570b1f9eb088176ffe0890
samir4ik86 <samir4ik86@gmail.com>
2017-06-26 21:10:58 +0400

    5

--- Knijka Struct/Knijka Struct/Source.cpp
+++ Knijka Struct/Knijka Struct/Source.cpp
@@ -1,7 +1,63 @@
 #include <iostream>
+#include <Windows.h>
+#define COORDS(row, col) SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), { (short)row, (short)col})
 using namespace std;
 
 void main()
 {
+	int operation = 0;
+	short def_col = 13;
+	while (true)
+	{
+		COORDS(60, 0);
+		cout << "*****Contact Book.*****\n\n\n";
+		COORDS(60, 2);
+		cout << "Select an action:\n\n";
+		COORDS(60, 3);
+		cout << "1. Display All Contacts.\n";
+		COORDS(60, 4);
+		cout << "2. Add new Contact.\n";
+		COORDS(60, 5);
+		cout << "3. Delete Contact.\n";
+		COORDS(60, 6);
+		cout << "4. Edit Contact.\n";
+		COORDS(60, 7);
+		cout << "5. Sort Contacts by Name.\n";
+		COORDS(60, 8);
+		cout << "6. Search Contact.\n";
+		COORDS(60, 9);
+		cout << "7. Exit.\n";
+		COORDS(0, def_col);
 
+		cin >> operation;
+		cin.ignore();
+
+		if (operation == 1)
+		{
+		}
+		else if (operation == 2)
+		{
+			
+		}
+		else if (operation == 3)
+		{
+			
+		}
+		else if (operation == 4)
+		{
+		
+		}
+		else if (operation == 5)
+		{
+		
+		}
+		else if (operation == 6)
+		{
+		
+		}
+		else if (operation == 7)
+		{
+	
+		}
+	}
 }
