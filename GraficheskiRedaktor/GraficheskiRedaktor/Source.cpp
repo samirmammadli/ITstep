03064a0f740dfcf7e31250ecf0d0317e6646b605
Samir <Samir4ik86@gmail.com>
2017-07-10 15:51:23 +0400

    1

--- GraficheskiRedaktor/GraficheskiRedaktor/Source.cpp
+++ GraficheskiRedaktor/GraficheskiRedaktor/Source.cpp
@@ -1,5 +1,15 @@
 #include "Functions.h"
 
+
+void hideCursor(bool switch_cursor = false)
+{
+	CONSOLE_CURSOR_INFO info;
+	info.bVisible = switch_cursor;
+	info.dwSize = 1;
+	SetConsoleCursorInfo(GetStdHandle(STD_OUTPUT_HANDLE), &info);
+}
+
+
 void main()
 {
 	srand(time(NULL));
@@ -12,15 +22,16 @@ void main()
 	for (int i = 0; i < size; i++)
 	{
 		field[i] = new matrix;
-		/*field[i]->bg = rand() % 11 + 5;
-		field[i]->fg = rand() % 11 + 5;
-		field[i]->symbol = rand() % 200 + 30;*/
+		field[i]->bg = rand() % 11 + 5;
+		field[i]->fg = rand() % 11 + 5; 
+		field[i]->symbol = rand() % 200 + 30;
 	}
-	int kursor;
+	int key, index = 0;
 	int a = 3, ax = a;
 	int b = 8, bx = b;
 	while (true)
 	{
+		hideCursor();
 		for (int i = 0; i < size; i++, b++)
 		{
 			COORDS(a, b);
@@ -36,36 +47,47 @@ void main()
 		cout << endl;
 		a = 3, b = 8;
 		COORDS(ax, bx);
-		kursor = getch();
-		if (kursor == 224)
+		hideCursor(true);
+		key = getch();
+		if (key == 224)
 		{
-			kursor = getch();
-			if (kursor == 72)
+			key = getch();
+			if (key == 72)
 			{
 				if (ax > 3)
 				{
 					ax--;
+					index -= col;
 				}
 			}
-			else if (kursor == 80)
+			else if (key == 80)
 			{
 				if (ax < row + a-1)
 				{
 					ax++;
+					index += col;
 				}
 			}
-			else if (kursor == 77)
+			else if (key == 77)
 			{
 				if (bx < col + b-1)
 				{
 					bx++;
+					index++;
 				}
 			}
-			else if (kursor == 75)
+			else if (key == 75)
 			{
 				if (bx > b)
+				{
 					bx--;
+					index--;
+				}	
 			}
+		}
+		else
+		{
+			field[index]->symbol = key;
 		}
 	}
 	
