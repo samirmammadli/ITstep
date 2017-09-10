b1f008bee21ea6a85fe938200f623a6c6cd9a976
samir4ik86 <samir4ik86@gmail.com>
2017-09-10 23:49:32 +0400

    1

--- Mario/Mario/Source.cpp
+++ Mario/Mario/Source.cpp
@@ -1,315 +1,59 @@
 ﻿#include <SFML/Graphics.hpp>
-#include <SFML/Audio.hpp>
-#include <string>
-using namespace std;
-
+#include <iostream>
 using namespace sf;
-
-
-
-
-
-float offsetX = 0, offsetY = 0;
-
-
-const int H = 25;   //17
-const int W = 150;
-
-
-String TileMap[H] = {
-	"000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000",
-	"0                                                                                                                                                    0",
-	"0                                                                                                                                                    0",
-	"0                                                                                                                                                    0",
-	"0                                                                                                                                                    0",
-	"0                                                                                                                                                    0",
-	"0                                                                                                                                                    0",
-	"0                                                                                                                                                    0",
-	"0                                                                                                                                                    0",
-	"0                                                                                                                                                    0",
-	"0                                                                                    w                                                               0",
-	"0                   w                                  w                   w                                                                         0",
-	"0                                      w                                       kk                                                                    0",
-	"0                                                                             k  k    k    k                                                         0",
-	"0                      c                                                      k      kkk  kkk  w                                                     0",
-	"0                                                                       r     k       k    k                                                         0",
-	"0                                                                      rr     k  k                r                                                  0",
-	"0                                                                     rrr      kk                rrr                                                 0",
-	"0              kc    k kck                                           rrrr                       rrrrr                                                0",
-	"0                                      t0                           rrrrr                      rrrrrrr                                               0",
-	"0G                                     00              t0          rrrrrr            G        rrrrrrrrr                                              0",
-	"0           d    g       d             00              00         rrrrrrr                    rrrrrrrrrrr                                             0",
-	"PPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPP  ",
-	"PPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPP  ",
-	"PPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPP0 ",
-};
-
-
-
-
-class PLAYER {
-
-public:
-
-	float dx, dy;
-	FloatRect rect;
-	bool onGround;
-	Sprite sprite;
-	float currentFrame;
-
-	PLAYER(Texture &image)
-	{
-		sprite.setTexture(image);
-		rect = FloatRect(100, 180, 16, 16);
-
-		dx = dy = 0.1;
-		currentFrame = 0;
-	}
-
-
-	void update(float time)
-	{
-
-		rect.left += dx * time;
-		Collision(0);
-
-
-		if (!onGround) dy = dy + 0.0005*time;
-		rect.top += dy*time;
-		onGround = false;
-		Collision(1);
-
-
-		currentFrame += time * 0.005;
-		if (currentFrame > 3) currentFrame -= 3;
-
-
-		if (dx>0) sprite.setTextureRect(IntRect(112 + 31 * int(currentFrame), 144, 16, 16));
-		if (dx<0) sprite.setTextureRect(IntRect(112 + 31 * int(currentFrame) + 16, 144, -16, 16));
-
-
-		sprite.setPosition(rect.left - offsetX, rect.top - offsetY);
-
-		dx = 0;
-	}
-
-
-	void Collision(int num)
-	{
-
-		for (int i = rect.top / 16; i<(rect.top + rect.height) / 16; i++)
-			for (int j = rect.left / 16; j<(rect.left + rect.width) / 16; j++)
-			{
-				if ((TileMap[i][j] == 'P') || (TileMap[i][j] == 'k') || (TileMap[i][j] == '0') || (TileMap[i][j] == 'r') || (TileMap[i][j] == 't'))
-				{
-					if (dy>0 && num == 1)
-					{
-						rect.top = i * 16 - rect.height;  dy = 0;   onGround = true;
-					}
-					if (dy<0 && num == 1)
-					{
-						rect.top = i * 16 + 16;   dy = 0;
-					}
-					if (dx>0 && num == 0)
-					{
-						rect.left = j * 16 - rect.width;
-					}
-					if (dx<0 && num == 0)
-					{
-						rect.left = j * 16 + 16;
-					}
-				}
-
-				if (TileMap[i][j] == 'c') {
-					//TileMap[i][j]=' '; 
-				}
-			}
-
-	}
-
-};
-
-
-
-class ENEMY
-{
-
-public:
-	float dx, dy;
-	FloatRect rect;
-	Sprite sprite;
-	float currentFrame;
-	bool life;
-
-
-	void set(Texture &image, int x, int y)
-	{
-		sprite.setTexture(image);
-		rect = FloatRect(x, y, 16, 16);
-
-		dx = 0.05;
-		currentFrame = 0;
-		life = true;
-	}
-
-	void update(float time)
-	{
-		rect.left += dx * time;
-
-		Collision();
-
-
-		currentFrame += time * 0.005;
-		if (currentFrame > 2) currentFrame -= 2;
-
-		sprite.setTextureRect(IntRect(18 * int(currentFrame), 0, 16, 16));
-		if (!life) sprite.setTextureRect(IntRect(58, 0, 16, 16));
-
-
-		sprite.setPosition(rect.left - offsetX, rect.top - offsetY);
-
-	}
-
-
-	void Collision()
-	{
-
-		for (int i = rect.top / 16; i<(rect.top + rect.height) / 16; i++)
-			for (int j = rect.left / 16; j<(rect.left + rect.width) / 16; j++)
-				if ((TileMap[i][j] == 'P') || (TileMap[i][j] == '0'))
-				{
-					if (dx>0)
-					{
-						rect.left = j * 16 - rect.width; dx *= -1;
-					}
-					else if (dx<0)
-					{
-						rect.left = j * 16 + 16;  dx *= -1;
-					}
-
-				}
-	}
-
-};
-
-
-
 int main()
 {
 
-
-
-	/*Clock ujal;
-	int a  = ujal.getElapsedTime().asMilliseconds();*/
-
-	RenderWindow window(VideoMode(800, 400), "Shmario");
-
-	Texture tileSet;
-	tileSet.loadFromFile("Mario_Tileset.png");
-
-
-	PLAYER Mario(tileSet);
-	ENEMY  enemy;
-	enemy.set(tileSet, 48 * 16, 21 * 16); // 13
-
-
-	Sprite tile(tileSet);
-
-	SoundBuffer buffer;
-	buffer.loadFromFile("Jump.ogg");
-	Sound sound(buffer);
-
-	Music music;
-	music.openFromFile("Mario_Theme.ogg");
-	music.play();
-
 	Clock clock;
-	float time;
+	RenderWindow window(sf::VideoMode(640, 480), "Lesson 5. kychka-pc.ru");
 
+
+
+	Texture herotexture;
+	herotexture.loadFromFile("images/hero.png");
+
+	Sprite herosprite;
+	herosprite.setTexture(herotexture);
+	herosprite.setTextureRect(IntRect(0, 192, 96, 96));//получили нужный нам прямоугольник с котом
+	herosprite.setPosition(250, 250); //выводим спрайт в позицию x y 
+
+	float CurrentFrame = 0;
+	float time;
 	while (window.isOpen())
 	{
+		time = clock.getElapsedTime().asMicroseconds(); //дать прошедшее время в микросекундах
+		clock.restart(); //перезагружает время
+		time  = time / 200; //скорость игры
 
-		time = clock.getElapsedTime().asMicroseconds();
-		clock.restart();
-
-		time /= 600;  // çäåñü ðåãóëèðóåì ñêîðîñòü èãðû
-
-		if (time > 20) time = 20;
-
-		Event event;
-		while (window.pollEvent(event))
-		{
-			if (event.type == Event::Closed)
-				window.close();
-		}
-
-
-		if (Keyboard::isKeyPressed(Keyboard::A))    Mario.dx = -0.1;
-
-		if (Keyboard::isKeyPressed(Keyboard::D))    Mario.dx = 0.1;
-
-		if (Keyboard::isKeyPressed(Keyboard::W))	if (Mario.onGround) { Mario.dy = -0.27; Mario.onGround = false;  sound.play(); }
-
-
-
-		Mario.update(time);
-		enemy.update(time);
-
-
-		if (Mario.rect.intersects(enemy.rect))
-		{
-			if (enemy.life) {
-				if (Mario.dy>0) { enemy.dx = 0; Mario.dy = -0.2; enemy.life = false; }
-				else Mario.sprite.setColor(Color::Red);
-			}
-		}
-
-
-		if (Mario.rect.left>200) offsetX = Mario.rect.left - 200;           //ñìåùåíèå
-
-
-
-
-		window.clear(Color(107, 140, 255));
-
-		for (int i = 0; i<H; i++)
-			for (int j = 0; j<W; j++)
-			{
-				if (TileMap[i][j] == 'P')  tile.setTextureRect(IntRect(143 - 16 * 3, 112, 16, 16));
-
-				if (TileMap[i][j] == 'k')  tile.setTextureRect(IntRect(143, 112, 16, 16));
-
-				if (TileMap[i][j] == 'c')  tile.setTextureRect(IntRect(143 - 16, 112, 16, 16));
-
-				if (TileMap[i][j] == 't')  tile.setTextureRect(IntRect(0, 47, 32, 95 - 47));
-
-				if (TileMap[i][j] == 'g')  tile.setTextureRect(IntRect(0, 16 * 9 - 5, 3 * 16, 16 * 2 + 5));
-
-				if (TileMap[i][j] == 'G')  tile.setTextureRect(IntRect(145, 222, 222 - 145, 255 - 222));
-
-				if (TileMap[i][j] == 'd')  tile.setTextureRect(IntRect(0, 106, 74, 127 - 106));
-
-				if (TileMap[i][j] == 'w')  tile.setTextureRect(IntRect(99, 224, 140 - 99, 255 - 224));
-
-				if (TileMap[i][j] == 'r')  tile.setTextureRect(IntRect(143 - 32, 112, 16, 16));
-
-				if ((TileMap[i][j] == ' ') || (TileMap[i][j] == '0')) continue;
-
-				tile.setPosition(j * 16 - offsetX, i * 16 - offsetY);
-				window.draw(tile);
-			}
-
-
-
+		std::cout << time << "\n";//смотрим как живет время (перезагружается, как видим)
+		sf::Event event;
+		window.pollEvent(event);
 		
-		window.draw(enemy.sprite);
-		window.draw(Mario.sprite);
+			if (event.type == sf::Event::Closed)
+				window.close();
 
+				if ((Keyboard::isKeyPressed(Keyboard::Left) || (Keyboard::isKeyPressed(Keyboard::A)))) { //если нажата клавиша стрелка влево или англ буква А
+					CurrentFrame += 0.002*time; //служит для прохождения по "кадрам". переменная доходит до трех суммируя произведение времени и скорости. изменив 0.005 можно изменить скорость анимации
+					if (CurrentFrame > 3) CurrentFrame -= 3; // если пришли к третьему кадру - откидываемся назад.
+					herosprite.setTextureRect(IntRect(96 * int(CurrentFrame), 96, 96, 96)); //проходимся по координатам Х. получается начинаем рисование с координаты Х равной 0,96,96*2, и опять 0
+					herosprite.move(-0.1*time, 0);//происходит само движение персонажа влево
+				}
+		if (Keyboard::isKeyPressed(Keyboard::Right)) { herosprite.move(0.1*time, 0); herosprite.setTextureRect(IntRect(0, 192, 96, 96)); } //координата Y, на которой герой изображен идущем вправо равна 96+96=192
+		if (Keyboard::isKeyPressed(Keyboard::Up)) { herosprite.move(0, -0.1*time); herosprite.setTextureRect(IntRect(0, 288, 96, 96)); } //координата Y на которой герой изображен идущим вверх равна 288
+		if (Keyboard::isKeyPressed(Keyboard::Down)) { herosprite.move(0, 0.1*time); herosprite.setTextureRect(IntRect(0, 0, 96, 96)); } //координата 0, это верхняя часть изображения с героем, от нее и отталкиваемся ровными квадратиками по 96. 
+
+			/*if (sf::Mouse::isButtonPressed(sf::Mouse::Left))
+			{
+				herosprite.setColor(Color::Red);
+			}
+			if (sf::Mouse::isButtonPressed(sf::Mouse::Right))
+			{
+				herosprite.setColor(Color::Color::Transparent);
+			}*/
+		window.clear();
+		window.draw(herosprite);
 		window.display();
 	}
 
 	return 0;
 }
-
-
-
