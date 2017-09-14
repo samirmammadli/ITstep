f6a07cdfdabc8d55260408284396cb77f0cdaf3f
Samir <Samir4ik86@gmail.com>
2017-09-14 17:23:43 +0400

    1

--- Mario/Mario/Source.cpp
+++ Mario/Mario/Source.cpp
@@ -2,16 +2,43 @@
 #include <iostream>
 #include <conio.h>
 using namespace sf;
+
+
+
+
+class Figures {
+	Image image;
+	Texture block_texture;
+	Sprite* figures = nullptr;
+public:
+	Figures()
+	{
+		this->image.loadFromFile("images/figures.jpg");
+		this->block_texture.loadFromImage(image);
+		this->figures = new Sprite[4];
+		for (int i = 0; i < 4; i++)
+		{
+			figures[i].setTexture(block_texture);
+		}
+	}
+};
+
+
+
 int main()
 {
 	Clock clock;
-	Clock timing;
-	RenderWindow window(sf::VideoMode(640, 480), "Lesson 5. kychka-pc.ru");
+	RenderWindow window(sf::VideoMode(800, 600), "Lesson 5. kychka-pc.ru");
 	int a = 0;
 
+
+
 	Image image;
-	image.loadFromFile("images/snake.png");
-	//image.createMaskFromColor(Color(41, 33, 59));
+	image.loadFromFile("images/figures.jpg");
+
+
+
+	//image.createMaskFromColor(Color(255, 255, 255));
 	/*image.createMaskFromColor(Color(199, 184, 186));
 	image.createMaskFromColor(Color(41, 33, 59));
 	image.createMaskFromColor(Color(143, 130, 143));
@@ -24,26 +51,24 @@ int main()
 
 	Sprite herosprite;
 	herosprite.setTexture(herotexture);
-	herosprite.setTextureRect(IntRect(190, 65, 65, 62));//получили нужный нам прямоугольник с котом
+	//herosprite.setTextureRect(IntRect(0, 0, 30, 30));//получили нужный нам прямоугольник с котом
 	herosprite.setPosition(0, 50); //выводим спрайт в позицию x y 
-	herosprite.setScale(0.5, 0.5);
+	//herosprite.setScale(0.5, 0.5);
 	
 	std::cout << herosprite.getPosition().x << std::endl;
 	std::cout << herosprite.getPosition().y << std::endl;
-
 	float CurrentFrame = 0;
 	float time;
 	while (window.isOpen())
+		
 	{
+
 		time = clock.getElapsedTime().asMicroseconds(); //дать прошедшее время в микросекундах
 
 		
 		clock.restart(); //перезагружает время
 		time = time / 4000; //скорость игры
-		
 
-		 if (int(timing.getElapsedTime().asSeconds()) > a) std::cout << int(timing.getElapsedTime().asSeconds()) << "\n";//смотрим как живет время (перезагружается, как видим)
-		 a = timing.getElapsedTime().asSeconds();
 		sf::Event event;
 		window.pollEvent(event);
 
@@ -53,29 +78,29 @@ int main()
 		if ((Keyboard::isKeyPressed(Keyboard::Left) || (Keyboard::isKeyPressed(Keyboard::A)))) { //если нажата клавиша стрелка влево или англ буква А
 			CurrentFrame += 0.02*time; //служит для прохождения по "кадрам". переменная доходит до трех суммируя произведение времени и скорости. изменив 0.005 можно изменить скорость анимации
 			if (CurrentFrame > 3) CurrentFrame -= 3; // если пришли к третьему кадру - откидываемся назад.
-			herosprite.setTextureRect(IntRect(190, 65, 65, 62)); //проходимся по координатам Х. получается начинаем рисование с координаты Х равной 0,96,96*2, и опять 0
+			//herosprite.setTextureRect(IntRect(190, 65, 65, 62)); //проходимся по координатам Х. получается начинаем рисование с координаты Х равной 0,96,96*2, и опять 0
 			herosprite.getPosition().x >= 0 ? herosprite.move(-time, 0) : 0;//происходит само движение персонажа влево
 		}
 		if (Keyboard::isKeyPressed(Keyboard::Right) || (Keyboard::isKeyPressed(Keyboard::D)))
 		{
 			CurrentFrame += 0.02*time; //служит для прохождения по "кадрам". переменная доходит до трех суммируя произведение времени и скорости. изменив 0.005 можно изменить скорость анимации
 			if (CurrentFrame > 3) CurrentFrame -= 3;
-			herosprite.setTextureRect(IntRect(255, 0, 65, 64));
-			herosprite.getPosition().x <= 608 ? herosprite.move(time, 0) : 0;// herosprite.setTextureRect(IntRect(0, 192, 96, 96)); //координата Y, на которой герой изображен идущем вправо равна 96+96=192
+			//herosprite.setTextureRect(IntRect(255, 0, 65, 64));
+			herosprite.getPosition().x <= 770 ? herosprite.move(time, 0) : 0;// herosprite.setTextureRect(IntRect(0, 192, 96, 96)); //координата Y, на которой герой изображен идущем вправо равна 96+96=192
 		} 
 		if (Keyboard::isKeyPressed(Keyboard::Up) || (Keyboard::isKeyPressed(Keyboard::W)))
 		{
 			CurrentFrame += 0.02*time; //служит для прохождения по "кадрам". переменная доходит до трех суммируя произведение времени и скорости. изменив 0.005 можно изменить скорость анимации
 			if (CurrentFrame > 3) CurrentFrame -= 3;
-			herosprite.setTextureRect(IntRect(190, 0, 65, 65));
+			//herosprite.setTextureRect(IntRect(190, 0, 65, 65));
 			herosprite.getPosition().y >= 0 ? herosprite.move(0, -time) : 0;// herosprite.setTextureRect(IntRect(0, 192, 96, 96)); //координата Y, на которой герой изображен идущем вправо равна 96+96=192
 		}
 		if (Keyboard::isKeyPressed(Keyboard::Down) || (Keyboard::isKeyPressed(Keyboard::S)))
 		{
 			CurrentFrame += 0.02*time; //служит для прохождения по "кадрам". переменная доходит до трех суммируя произведение времени и скорости. изменив 0.005 можно изменить скорость анимации
 			if (CurrentFrame > 3) CurrentFrame -= 3;
-			herosprite.setTextureRect(IntRect(258, 65, 65, 64));
-			herosprite.getPosition().y < 445 ?  herosprite.move(0, time) : 0;// herosprite.setTextureRect(IntRect(0, 192, 96, 96)); //координата Y, на которой герой изображен идущем вправо равна 96+96=192
+			//herosprite.setTextureRect(IntRect(258, 65, 65, 64));
+			herosprite.getPosition().y < 570 ?  herosprite.move(0, time) : 0;// herosprite.setTextureRect(IntRect(0, 192, 96, 96)); //координата Y, на которой герой изображен идущем вправо равна 96+96=192
 		}
 			/*if (sf::Mouse::isButtonPressed(sf::Mouse::Left))
 			{
