23961b05dd09fd85e7b461a2161632d1fbde19d0
Samir <Samir4ik86@gmail.com>
2017-09-15 17:57:07 +0400

    1

--- Mario/Mario/Source.cpp
+++ Mario/Mario/Source.cpp
@@ -29,12 +29,13 @@ int main()
 {
 	Clock clock;
 	RenderWindow window(sf::VideoMode(800, 600), "Lesson 5. kychka-pc.ru");
+	
 	int a = 0;
 
 
 
 	Image image;
-	image.loadFromFile("images/figures.jpg");
+	image.loadFromFile("images/tiles.png");
 
 	bool pause = false;
 	//image.createMaskFromColor(Color(255, 255, 255));
@@ -44,85 +45,151 @@ int main()
 
 	Sprite herosprite;
 	herosprite.setTexture(herotexture);
-	//herosprite.setTextureRect(IntRect(0, 0, 30, 30));//получили нужный нам прямоугольник с котом
-	herosprite.setPosition(0, 50); //выводим спрайт в позицию x y 
+	herosprite.setTextureRect(IntRect(0, 0, 16, 16));//получили нужный нам прямоугольник с котом
+	herosprite.setPosition(400, 0); //выводим спрайт в позицию x y 
 	//herosprite.setScale(0.5, 0.5);
-	herosprite.setColor(Color(0, 255, 255));
+	//herosprite.setColor(Color(0, 255, 255));
 
 
 	std::cout << herosprite.getPosition().x << std::endl;
 	std::cout << herosprite.getPosition().y << std::endl;
 	float CurrentFrame = 0;
-	float time;
+	float time = 0;
 	sf::Event event;
 	while (window.isOpen())	
 	{
-		if (!pause)
-		{
-			time = clock.getElapsedTime().asMicroseconds(); //дать прошедшее время в микросекундах
 
+		window.setKeyRepeatEnabled(false);
+			time = clock.getElapsedTime().asSeconds(); //дать прошедшее время в микросекундах
 			clock.restart(); //перезагружает время
-			time = time / 4000; //скорость игры
+			time *= 100;
+			
+			
+			(window.pollEvent(event));
+			
+				if (event.type == sf::Event::Closed)
+					window.close();
+				else if (event.type == sf::Event::KeyPressed)
+				{
+					if (event.key.code == sf::Keyboard::A)
+						herosprite.getPosition().x > 0 ? herosprite.move(-15, 0) : 0;
+					else if (event.key.code == sf::Keyboard::D)
+						herosprite.getPosition().x < 770 ? herosprite.move(15, 0) : 0;
+					else if (event.key.code == sf::Keyboard::W)
+						herosprite.getPosition().y > 0 ? herosprite.move(0, -25) : 0;
+				}
 
 			
-			window.pollEvent(event);
 
-			if (event.type == sf::Event::Closed)
-				window.close();
+		
+			
+			
+			herosprite.getPosition().y < 570 ? (herosprite.move(0, time)) : 0;//происходит само движение персонажа влево
+			/*if (Keyboard::isKeyPressed(Keyboard::A) || (Keyboard::isKeyPressed(Keyboard::Left)))
+				herosprite.getPosition().x > 0 ? herosprite.move(-time, 0) : 0;*/
+			
 
-			if ((Keyboard::isKeyPressed(Keyboard::Left) || (Keyboard::isKeyPressed(Keyboard::A)))) { //если нажата клавиша стрелка влево или англ буква А
-				CurrentFrame += 0.02*time; //служит для прохождения по "кадрам". переменная доходит до трех суммируя произведение времени и скорости. изменив 0.005 можно изменить скорость анимации
-				if (CurrentFrame > 3) CurrentFrame -= 3; // если пришли к третьему кадру - откидываемся назад.
-														 //herosprite.setTextureRect(IntRect(190, 65, 65, 62)); //проходимся по координатам Х. получается начинаем рисование с координаты Х равной 0,96,96*2, и опять 0
-				herosprite.getPosition().x >= 0 ? herosprite.move(-time, 0) : 0;//происходит само движение персонажа влево
-			}
-			if (Keyboard::isKeyPressed(Keyboard::Right) || (Keyboard::isKeyPressed(Keyboard::D)))
-			{
-				CurrentFrame += 0.02*time; //служит для прохождения по "кадрам". переменная доходит до трех суммируя произведение времени и скорости. изменив 0.005 можно изменить скорость анимации
-				if (CurrentFrame > 3) CurrentFrame -= 3;
-				//herosprite.setTextureRect(IntRect(255, 0, 65, 64));
-				herosprite.getPosition().x <= 770 ? herosprite.move(time, 0) : 0;// herosprite.setTextureRect(IntRect(0, 192, 96, 96)); //координата Y, на которой герой изображен идущем вправо равна 96+96=192
-			}
-			if (Keyboard::isKeyPressed(Keyboard::Up) || (Keyboard::isKeyPressed(Keyboard::W)))
-			{
-				CurrentFrame += 0.02*time; //служит для прохождения по "кадрам". переменная доходит до трех суммируя произведение времени и скорости. изменив 0.005 можно изменить скорость анимации
-				if (CurrentFrame > 3) CurrentFrame -= 3;
-				//herosprite.setTextureRect(IntRect(190, 0, 65, 65));
-				herosprite.getPosition().y >= 0 ? herosprite.move(0, -time) : 0;// herosprite.setTextureRect(IntRect(0, 192, 96, 96)); //координата Y, на которой герой изображен идущем вправо равна 96+96=192
-			}
-			if (Keyboard::isKeyPressed(Keyboard::Down) || (Keyboard::isKeyPressed(Keyboard::S)))
-			{
-				CurrentFrame += 0.02*time; //служит для прохождения по "кадрам". переменная доходит до трех суммируя произведение времени и скорости. изменив 0.005 можно изменить скорость анимации
-				if (CurrentFrame > 3) CurrentFrame -= 3;
-				//herosprite.setTextureRect(IntRect(258, 65, 65, 64));
-				herosprite.getPosition().y < 570 ? herosprite.move(0, time) : 0;// herosprite.setTextureRect(IntRect(0, 192, 96, 96)); //координата Y, на которой герой изображен идущем вправо равна 96+96=192
-			}
-			if (Keyboard::isKeyPressed(Keyboard::Space))
-			{
-				pause = true;
-			}
-			/*if (sf::Mouse::isButtonPressed(sf::Mouse::Left))
-			{
-			herosprite.setColor(Color::Red);
-			}
-			if (sf::Mouse::isButtonPressed(sf::Mouse::Right))
-			{
-			herosprite.setColor(Color::Color::Transparent);
-			}*/
+		
 			window.clear();
 			window.draw(herosprite);
 			window.display();
-		}
-		else
-		{
-			if (Keyboard::isKeyPressed(Keyboard::Space))
-			{
-				pause = false;
-				clock.restart();
-			}
-		}
-		
+			
 	}
 
 	return 0;
+	
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//if ((Keyboard::isKeyPressed(Keyboard::Left) || (Keyboard::isKeyPressed(Keyboard::A)))) { //если нажата клавиша стрелка влево или англ буква А
+//	CurrentFrame += 0.02*time; //служит для прохождения по "кадрам". переменная доходит до трех суммируя произведение времени и скорости. изменив 0.005 можно изменить скорость анимации
+//	if (CurrentFrame > 3) CurrentFrame -= 3; // если пришли к третьему кадру - откидываемся назад.
+//											 //herosprite.setTextureRect(IntRect(190, 65, 65, 62)); //проходимся по координатам Х. получается начинаем рисование с координаты Х равной 0,96,96*2, и опять 0
+//	time >= 0.015 && herosprite.getPosition().x >= 0 ? (herosprite.move(-10, 0), time = 0) : 0;//происходит само движение персонажа влево
+//}
+//if (Keyboard::isKeyPressed(Keyboard::Right) || (Keyboard::isKeyPressed(Keyboard::D)))
+//{
+//	CurrentFrame += 0.02*time; //служит для прохождения по "кадрам". переменная доходит до трех суммируя произведение времени и скорости. изменив 0.005 можно изменить скорость анимации
+//	if (CurrentFrame > 3) CurrentFrame -= 3;
+//	//herosprite.setTextureRect(IntRect(255, 0, 65, 64));
+//	herosprite.getPosition().x <= 770 ? herosprite.move(time, 0) : 0;// herosprite.setTextureRect(IntRect(0, 192, 96, 96)); //координата Y, на которой герой изображен идущем вправо равна 96+96=192
+//}
+//if (Keyboard::isKeyPressed(Keyboard::Up) || (Keyboard::isKeyPressed(Keyboard::W)))
+//{
+//	CurrentFrame += 0.02*time; //служит для прохождения по "кадрам". переменная доходит до трех суммируя произведение времени и скорости. изменив 0.005 можно изменить скорость анимации
+//	if (CurrentFrame > 3) CurrentFrame -= 3;
+//	//herosprite.setTextureRect(IntRect(190, 0, 65, 65));
+//	herosprite.getPosition().y >= 0 ? herosprite.move(0, -time) : 0;// herosprite.setTextureRect(IntRect(0, 192, 96, 96)); //координата Y, на которой герой изображен идущем вправо равна 96+96=192
+//}
+//if (Keyboard::isKeyPressed(Keyboard::Down) || (Keyboard::isKeyPressed(Keyboard::S)))
+//{
+//	CurrentFrame += 0.02*time; //служит для прохождения по "кадрам". переменная доходит до трех суммируя произведение времени и скорости. изменив 0.005 можно изменить скорость анимации
+//	if (CurrentFrame > 3) CurrentFrame -= 3;
+//	//herosprite.setTextureRect(IntRect(258, 65, 65, 64));
+//	herosprite.getPosition().y < 570 ? herosprite.move(0, time) : 0;// herosprite.setTextureRect(IntRect(0, 192, 96, 96)); //координата Y, на которой герой изображен идущем вправо равна 96+96=192
+//}
+//if (Keyboard::isKeyPressed(Keyboard::Space))
+//{
+//	pause = true;
+//}
