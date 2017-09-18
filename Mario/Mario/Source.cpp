949d1aa05a5c6ded37c9e103994bb6d110fe6c6d
samir4ik86 <samir4ik86@gmail.com>
2017-09-19 01:32:41 +0400

    1

--- Mario/Mario/Source.cpp
+++ Mario/Mario/Source.cpp
@@ -5,7 +5,10 @@ using namespace sf;
 using namespace std;
 
 
-
+struct PointS {
+	int x;
+	int y;
+};
 
 class Figures {
 	Image image;
@@ -22,6 +25,55 @@ public:
 			figures[i].setTexture(block_texture);
 		}
 	}
+	void SetPosition(PointS coords[4])
+	{
+		for (int i = 0; i < 4; i++)
+		{
+			figures[i].setPosition(coords[i].x, coords[i].y);
+		}
+	}
+	Sprite& Getfig(int index)
+	{
+		return figures[index];
+	}
+
+	void move(int x, int y)
+	{
+		for (int i = 0; i < 4; i++)
+		{
+			figures[i].move(x, y);
+		}
+		
+	}
+	void rotate()
+	{
+		PointS b;
+		b.x = figures[1].getPosition().x;
+		b.y = figures[1].getPosition().y;
+		for (int i = 0; i < 4; i++)
+		{
+			if (i == 1) continue;
+			int x = figures[i].getPosition().x;
+			int y = figures[i].getPosition().y;
+
+			if (x < b.x && y < b.y)
+				figures[i].move(32, 0);
+			else if (x < b.x && y == b.y)
+				figures[i].move(16, -16);
+			else if (x < b.x && y > b.y)
+				figures[i].move(0, -32);
+			else if (x > b.x && y > b.y)
+				figures[i].move(-32, 0);
+			else if (x > b.x && y == b.y)
+				figures[i].move(-16, 16);
+			else if (x > b.x && y < b.y)
+				figures[i].move(0, 32);
+			else if (x == b.x && y < b.y)
+				figures[i].move(16, 16);
+			else if (x == b.x && y > b.y)
+				figures[i].move(-16, -16);
+		}
+	}
 };
 
 
@@ -30,10 +82,50 @@ int main()
 {
 	Clock clock;
 	RenderWindow window(sf::VideoMode(800, 600), "Lesson 5. kychka-pc.ru");
-	
+
 	int a = 0;
 
 
+	PointS * fig = new PointS[4];
+
+
+	//T
+	fig[0].x = 32;
+	fig[0].y = 16;
+
+	fig[1].x = 32;
+	fig[1].y = 32;
+
+	fig[2].x = 32;
+	fig[2].y = 48;
+
+	fig[3].x = 48;
+	fig[3].y = 32;
+
+	//Z
+	fig[0].x = 16;
+	fig[0].y = 32;
+
+	fig[2].x = 32;
+	fig[2].y = 16;
+
+	fig[3].x = 48;
+	fig[3].y = 16;
+
+
+	//L
+	fig[0].x = 32;
+	fig[0].y = 16;
+
+	fig[2].x = 32;
+	fig[2].y = 48;
+
+	fig[3].x = 16;
+	fig[3].y = 48;
+
+
+	Figures fig1;
+	fig1.SetPosition(fig);
 
 	Image image;
 	image.loadFromFile("images/tiles.png");
@@ -74,12 +166,13 @@ int main()
 					window.close();
 				else if (event.type == sf::Event::KeyPressed)
 				{
-					//if (event.key.code == sf::Keyboard::A)
-					//	herosprite.getPosition().x > 0 ? herosprite.move(-30, 0) : 0; //15
-					//else if (event.key.code == sf::Keyboard::D)
-					//	herosprite.getPosition().x < 770 ? herosprite.move(30, 0) : 0;
+					if (event.key.code == sf::Keyboard::A)
+						fig1.move(-30, 0);
+					else if (event.key.code == sf::Keyboard::D)
+						fig1.move(30, 0);
 					if (event.key.code == sf::Keyboard::W)
-						temp = delay -= 0.2;
+						fig1.rotate();
+						//temp = delay -= 0.2;
 				}
 
 			
@@ -87,7 +180,7 @@ int main()
 		
 			
 			
-			time > delay && herosprite.getPosition().y < 570 ? ((herosprite.move(0, 17)), time = 0) : 0;//происходит само движение персонажа влево
+			time > delay ? (fig1.move(0, 17), time = 0) : 0;//происходит само движение персонажа влево
 			//delay = 0;
 				
 			if (Keyboard::isKeyPressed(Keyboard::S) || (Keyboard::isKeyPressed(Keyboard::Down)))
@@ -102,7 +195,10 @@ int main()
 
 		
 			window.clear();
-			window.draw(herosprite);
+			for (int i = 0; i < 4; i++)
+			{
+				window.draw(fig1.Getfig(i));
+			}
 			window.display();
 			
 	}
