84a1ab5a6fda7e46876212fffe36117d9592957d
samir4ik86 <samir4ik86@gmail.com>
2017-09-19 21:11:44 +0400

    123

--- New/New/Source.cpp
+++ New/New/Source.cpp
@@ -38,6 +38,7 @@ struct point {
 
 void main()
 {
+	//Test a;
 	/*Test *p = Test::Obj();
 
 
@@ -79,3 +80,51 @@ void main()
 
 
 }
+
+
+
+
+
+
+
+//#include <iostream>
+//#include <memory>
+//using namespace std;
+//
+//class Fraction
+//{
+//private:
+//	int m_numerator = 0;
+//	int m_denominator = 1;
+//
+//public:
+//	Fraction(int numerator = 0, int denominator = 1) :
+//		m_numerator(numerator), m_denominator(denominator)
+//	{
+//	}
+//
+//	friend std::ostream& operator<<(std::ostream& out, const Fraction &f1)
+//	{
+//		out << f1.m_numerator << "/" << f1.m_denominator;
+//		return out;
+//	}
+//};
+//
+//void printFraction(const unique_ptr<Fraction> const &ptr)
+//{
+//	if (ptr)
+//		std::cout << *ptr << endl;
+//}
+//
+//int main()
+//{
+//
+//
+//	unique_ptr<Fraction> ptr(new Fraction(3, 5));
+//
+//	//Fraction *ptr = new Fraction(3, 5);
+//
+//	printFraction(ptr);
+//
+//	return 0;
+//}
