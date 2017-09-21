0d35cb8a8fbcc8fe0944063672e31a38e6107081
Samir <Samir4ik86@gmail.com>
2017-09-21 16:21:29 +0400

    1

---
+++ test2/test2/Source.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Student {
+public:
+	string name;
+	Student* next;
+	string surname;
+	Student(string name = "", string surname = "") {
+		this->name = name;
+		this->surname = surname;
+		next = nullptr;
+	}
+};
+
+class StudentArray {
+	Student* head;
+public:
+	StudentArray(string name = "", string surname = "") {
+		head = new Student;
+		head->name = name;
+		head->surname = surname;
+		head->next = nullptr;
+	}
+	void addStudent(string name, string surname) {
+		Student* temp = head;
+		while (temp->next != nullptr) {
+			temp = temp->next;
+		}
+		temp->next = new Student(name, surname);
+	}
+	void show() {
+		Student* tmp = head;
+		while (tmp->next != nullptr) {
+			cout << tmp->name << " " << tmp->surname << endl;
+			tmp = tmp->next;
+		}
+		cout << tmp->name << " " << tmp->surname << endl;
+	}
+	int getSize() {
+		int count = 0;
+		Student* temp = head;
+		while (temp->next != nullptr) {
+			count++;
+			temp = temp->next;
+		}
+		count++;
+		return count;
+	}
+};
+
+
+int main()
+{
+	StudentArray* classA = new StudentArray("Anar", "Babayev");
+	classA->addStudent("Rufat", "Gulabli");
+	classA->addStudent("Samir", "Mammadli");
+	classA->show();
+	cout << classA->getSize() << endl;
+
+	return 0;
+}
