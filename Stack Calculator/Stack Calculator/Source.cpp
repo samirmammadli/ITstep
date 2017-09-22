ee56aef176b95b4a4a8e9dc9cd8277fd65da1aa7
Samir <Samir4ik86@gmail.com>
2017-09-22 17:39:42 +0400

    1

--- Stack Calculator/Stack Calculator/Source.cpp
+++ Stack Calculator/Stack Calculator/Source.cpp
@@ -7,19 +7,15 @@ void test1(const Calculator s1)
 	0;
 }
 
-void main()
+
+int Calculate(Stack<char> &operators, Stack<double> &numbers, char* lesh, int i = 0)
 {
-	Calculator* test = Calculator::Obj();
-	cout << test->getMaxSize() << endl;
-	test1(*test);
-	int u;
-	char * lesh = new char[200];
-	cin.getline(lesh, 200);
-	Stack<double> numbers(200);
-	Stack<char> operators(200);
+	bool a = 0;
 	int size = strlen(lesh) + 1;
-	for (int i = 0; i < size; i++)
+	for (; i < size ; i++)
 	{
+		operators.peek() == '(' ? (operators.push('v'), i = Calculate(operators, numbers, lesh, i), printf("zalez\n")) : 0;
+		
 		if (lesh[i] >= 48 && lesh[i] <= 57)
 		{
 			int temp = 0;
@@ -32,9 +28,14 @@ void main()
 			i--;
 			numbers.push(temp);
 		}
-		else 
+		else
 		{
-			if (operators.peek() == '*' || operators.peek() == '/')
+			if (operators.peek() == ')')
+			{
+				operators.pop();
+				break;
+			}
+			 if (operators.peek() == '*' || operators.peek() == '/')
 			{
 				double temp = numbers.peek();
 				numbers.pop();
@@ -46,21 +47,39 @@ void main()
 			lesh[i] != '\0' ? operators.push(lesh[i]) : 0;
 		}
 	}
-
-	double total = 0;
-	while (numbers.getCount() > 1)
+	while (numbers.getCount() > 1 && operators.peek() != '(')
 	{
 		double temp = numbers.peek();
 		operators.peek() == '-' ? temp *= -1 : 0;
 		operators.pop();
 		numbers.pop();
 		operators.peek() == '-' ? temp = (numbers.peek() * -1) + temp : temp = numbers.peek() + temp;
-		operators.pop();
 		numbers.pop();
 		numbers.push(temp);
+
+
+		operators.pop();
 		operators.push('+');
 	}
 
+	return i;
+}
+
+
+void main()
+{
+	Stack<double> numbers(200);
+	Stack<char> operators(200);
+	Calculator* test = Calculator::Obj();
+	cout << test->getMaxSize() << endl;
+	test1(*test);
+	char * lesh = new char[200];
+	cin.getline(lesh, 200);
+	
+	
+	 Calculate(operators, numbers, lesh);
+
+	 cout << numbers.getCount() << endl;
 
 	for (int i = 0; i < numbers.getCount(); i++)
 	{
@@ -68,9 +87,7 @@ void main()
 	}
 	cout << endl;
 	cout << "Symb : " << operators.arr << endl;
-	//cout << "Answer: " << total << endl;
 	cout << "Symb Size: " << operators.getCount() << endl;
-
 
 
 	
