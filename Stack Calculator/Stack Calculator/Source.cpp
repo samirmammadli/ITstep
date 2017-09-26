72d750b8efbfedd6cd117cf4fbbb839ba70553f2
Samir <Samir4ik86@gmail.com>
2017-09-26 12:24:04 +0400

    1

--- Stack Calculator/Stack Calculator/Source.cpp
+++ Stack Calculator/Stack Calculator/Source.cpp
@@ -9,7 +9,7 @@ void main()
 	while (true)
 	{
 		system("cls");
-		cout << "Input Expression:\n";
+		cout << "Input Expression:\nExample: 5.2*(-2.9+8)+4\n\n";
 		char *temp = new char[200];
 		cin.getline(temp, 200);
 		cout << "Total: " << test->inputExpression(temp) << endl;
