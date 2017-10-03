c2a81d7e46c3e2fbcf9ed66f34db7e9b008bf3f1
Samir <Samir4ik86@gmail.com>
2017-10-03 17:39:22 +0400

    1

--- File-Manager/File-Manager/Source.cpp
+++ File-Manager/File-Manager/Source.cpp
@@ -4,8 +4,38 @@
 #include <io.h>
 #include <vector>
 #include <Windows.h>
+#include <windows.h>
+#include <conio.h>
 #define COORDS(row, col) SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), { (short)row, (short)col})
+
+#define COLORS(fg, bg) SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), bg * 16 + fg)
+#define findclose _findclose
 using namespace std;
+
+
+
+
+
+enum Colors
+{
+	BLACK,
+	DARKBLUE,
+	DARKGREEN,
+	DARKCYAN,
+	DARKRED,
+	DARKMAGENTA,
+	BROWN,
+	LIGHTGREY,
+	DARKGREY,
+	BLUE,
+	GREEN,
+	CYAN,
+	RED,
+	MAGENTA,
+	YELLOW,
+	WHITE,
+	DEFAULT = 7
+};
 
 HANDLE h = GetStdHandle(STD_OUTPUT_HANDLE);
 
@@ -14,41 +44,44 @@ struct FileProp
 	string type;
 	string attr = "";
 	string size_type = "Bytes";
-	float size;
-	
+	double size;
 };
 
 class FileManager
 {
+	int textBufferSize = 25;
 	vector<string> buffer;
 	string path;
 	FileProp properties;
 	void getAttributes(const _finddata_t &file)
 	{
-		properties.size = (int(file.size*10) / 10);
-		if (properties.size < 1000)
-			properties.size_type = "Bytes";
-		else if (properties.size < 1000000)
+		properties.size = file.size;
+		properties.size_type = "B";
+
+		if (properties.size >= 1000)
 		{
 			properties.size /= 1000;
-			properties.size_type = "Kbytes";
+			properties.size_type = "Kb";
 		}
-		else
+		if (properties.size >= 1000)
 		{
-			properties.size /= 1000000;
-			properties.size_type = "Mbytes";
+			properties.size /= 1000;
+			properties.size_type = "Mb";
 		}
-
-		file.attrib & _A_SUBDIR ? properties.type = "|<DIR>|" : properties.type = "|     |";
+		if (properties.size >= 1000)
+		{
+			properties.size /= 1000;
+			properties.size_type = "Gb";
+		}
+		file.attrib & _A_SUBDIR ? properties.type = "|<DIR>|" : properties.type = "|file |";
 		properties.attr = "";
 		if (file.attrib & _A_RDONLY)  properties.attr += " R ";
 		if (file.attrib & _A_HIDDEN)  properties.attr += " H ";
 		if (file.attrib & _A_SYSTEM)  properties.attr += " S" ;
 		if (properties.attr.size() == 0) properties.attr = " Normal";
-
 	}
 public:
-	FileManager(string path = "C:\\*")
+	FileManager(string path = "C:\*")
 	{
 		this->path = path;
 	}
@@ -57,25 +90,51 @@ public:
 	{
 		_finddata_t fileinfo;
 		int handle = _findfirst(str.c_str(), &fileinfo);
-		int find = handle;
+		int find = -1;
+		if (handle != -1) find = _findnext(handle, &fileinfo);
 		int count = 0;
-		cout << "***************************************************************************************\n";
-		cout << "Name:                                        |Type:| Attr:   | Size:                  *\n";
-		cout << "***************************************************************************************\n";
+		
 		while (find != -1)
 		{
-			count++;
+			char temp[500];
 			getAttributes(fileinfo);
-			printf("%-45.34s%-4s%-9s| %.3g %s\n", fileinfo.name, properties.type.c_str(), properties.attr.c_str(), properties.size, properties.size_type.c_str());
-			cout << "---------------------------------------------------------------------------------------\n";
+			sprintf(temp, "%-45.34s%-4s%-9s| %-7.3g|%-3s|\n", fileinfo.name, properties.type.c_str(), properties.attr.c_str(), properties.size, properties.size_type.c_str());
+			buffer.push_back(temp);
 			find = _findnext(handle, &fileinfo);
+			count++;
 		}
-
-		cout << "\nInformation: was found " << count;
-		cout << " file(s) in folder..." << str << "\n\n";
-		_findclose(handle);
+		findclose(handle);
+		/*cout << "\nInformation: was found " << count;
+		cout << " file(s) in folder:  " << str << "\n\n";*/
+		print();
 	}
-	
+	void print()
+	{
+		cout << "***************************************************************************\n";
+		cout << "Name:                                        |Type:| Attr:   | Size:  |   |\n";
+		cout << "***************************************************************************\n";
+		int i = 0;
+		int index = 0;
+		SetConsoleCursorPosition(h, { 0, 3 });
+		while (buffer.size() != 0)
+		{
+			if (i == index) COLORS(BLUE, CYAN);
+			cout << buffer[i];
+			COLORS(CYAN, DARKBLUE);
+			i++;
+			if (i == buffer.size() - 1 || i % textBufferSize == 0) control(i, index);
+		}
+		
+	}
+	void control(int &i, int &index)
+	{
+		int key = _getch();
+		if (key == 'w' && index > 0) index--;
+		if (key == 's' && index < buffer.size()- 2) index++;
+		i = index - index % textBufferSize;
+		SetConsoleCursorPosition(h, { 0, 3 });
+
+	}
 	void findFiles(string mask);
 	void changeDirectory(string dir);
 	void remove(string path);
@@ -84,32 +143,10 @@ public:
 	void move();
 };
 
-//void deletepapka(string path, string word)
-//{
-//	string temp;
-//	int count = 0;
-//	_finddata_t fileinfo;
-//	int handle = _findfirst(path.c_str(), &fileinfo);
-//	int find = handle;
-//	while (find != -1)
-//	{
-//		if (fileinfo.attrib & _A_SUBDIR)
-//		{
-//			deletepapka(path);
-//		}
-//			
-//		temp = fileinfo.name;
-//		find = _findnext(handle, &fileinfo);
-//	}
-//	_findclose(handle);
-//	return 
-//}
-
-
-
 void main()
-{
-	
+{ 
+	system("color 1B");
+	setlocale(LC_ALL, "Russian");
 	//FileManager fm("C:\\Windows\\System32\\");
 	//fm.find("*.txt");
 	//fm.changeDirectory("Debug");
@@ -119,14 +156,13 @@ void main()
 	system("cls");
 	FileManager fm;
 	fm.showDirectory(str);
-	//setlocale(LC_ALL, "Russian");
+	
 
 	//rename("text.txt", str.c_str());
 	//remove(str.c_str());
 
 	//mkdir(str.c_str());
 	//rmdir(str.c_str());
-	rmdir("F:\papka");
 	
 
 }
