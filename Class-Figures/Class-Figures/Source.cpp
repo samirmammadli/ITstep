315ba02883ee1d08c6c98f19a8a8233136f61ea7
samir4ik86 <samir4ik86@gmail.com>
2017-10-14 02:17:07 +0400

    fe

--- Class-Figures/Class-Figures/Source.cpp
+++ Class-Figures/Class-Figures/Source.cpp
@@ -14,7 +14,20 @@ class Color
 	short green;
 	short blue;
 public:
+	
 	Color() : red(0), green(0), blue(0) {}
+	short getRed()
+	{
+		return red;
+	}
+	short getGreen()
+	{
+		return green;
+	}
+	short getBlue()
+	{
+		return blue;
+	}
 	void setColor(short r, short g, short b)
 	{
 		if (r <= 255 && g <= 255 && b <= 255)
@@ -54,6 +67,10 @@ protected:
 	Color bgcolor;
 	Color fgcolor;
 public:
+	Figure(int width = 0) : width(width)
+	{
+
+	}
 	void setBgColor(short r, short g, short b)
 	{
 		bgcolor.setColor(r,g,b);
@@ -78,12 +95,31 @@ public:
 	{
 		fgcolor.darken(value);
 	}
-	void draw();
+	void setWidth(int width)
+	{
+		this->width = width;
+	}
+	void setPosition(int x, int y)
+	{
+		position.x = x;
+		position.y = y;
+	}
+	void draw()
+	{
+		cout << "Position x: " << position.x << endl;
+		cout << "Position y: " << position.y << endl;
+		printf("Foreground color: #%02x%02x%02x\n", fgcolor.getRed(), fgcolor.getGreen(), fgcolor.getBlue());
+		printf("Background color: #%02x%02x%02x\n", bgcolor.getRed(), bgcolor.getGreen(), bgcolor.getBlue());
+	}
 };
 	
 class Square : public Figure
 {
 public:
+	Square() : Figure()
+	{
+
+	}
 	void draw();
 };
 	
