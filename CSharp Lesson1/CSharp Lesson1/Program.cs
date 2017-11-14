1e4bf6a3df78d8548677d5fc8bc24a371f3246e3
samir4ik86 <samir4ik86@gmail.com>
2017-11-14 21:53:58 +0400

    1

---
+++ CSharp Lesson1/CSharp Lesson1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_Lesson1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.SetCursorPosition(10,10);
+            Console.Title = "CHelovek";
+            int? x = null;
+            string hw = "Netersen?";
+            Console.WriteLine($"Salam ay{x}, {hw} brat!");
+            decimal xu = 74985792848357098275098247503m;
+        }
+    }
+}
