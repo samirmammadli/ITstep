ac4b9c87ef5c7e9b7201f5fa647e8bafb24cda00
samir4ik86 <samir4ik86@gmail.com>
2017-11-17 21:58:57 +0400

    1

--- DZ1/DZ1/Form1.Designer.cs
+++ DZ1/DZ1/Form1.Designer.cs
@@ -32,18 +32,16 @@
             this.button2 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.fileSystemWatcher1 = new System.IO.FileSystemWatcher();
-            this.tabControl1 = new System.Windows.Forms.TabControl();
-            this.tabPage1 = new System.Windows.Forms.TabPage();
-            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.DisplayMessagesBox = new System.Windows.Forms.TextBox();
+            this.ChatBox = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).BeginInit();
-            this.tabControl1.SuspendLayout();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(96, 97);
+            this.button1.Location = new System.Drawing.Point(699, 83);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(108, 23);
             this.button1.TabIndex = 0;
             this.button1.Text = "Log In";
             this.button1.UseVisualStyleBackColor = true;
@@ -51,9 +49,9 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(96, 126);
+            this.button2.Location = new System.Drawing.Point(699, 112);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.Size = new System.Drawing.Size(108, 23);
             this.button2.TabIndex = 1;
             this.button2.Text = "Registration";
             this.button2.UseVisualStyleBackColor = true;
@@ -64,7 +62,7 @@
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Rockwell", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.LightSeaGreen;
-            this.label1.Location = new System.Drawing.Point(9, 27);
+            this.label1.Location = new System.Drawing.Point(619, 9);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(263, 48);
             this.label1.TabIndex = 2;
@@ -78,50 +76,47 @@
             this.fileSystemWatcher1.SynchronizingObject = this;
             this.fileSystemWatcher1.Changed += new System.IO.FileSystemEventHandler(this.fileSystemWatcher1_Changed);
             // 
-            // tabControl1
+            // DisplayMessagesBox
             // 
-            this.tabControl1.Controls.Add(this.tabPage1);
-            this.tabControl1.Controls.Add(this.tabPage2);
-            this.tabControl1.Location = new System.Drawing.Point(1, -2);
-            this.tabControl1.Name = "tabControl1";
-            this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(449, 252);
-            this.tabControl1.TabIndex = 3;
+            this.DisplayMessagesBox.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.DisplayMessagesBox.Font = new System.Drawing.Font("Arial Unicode MS", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.DisplayMessagesBox.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.DisplayMessagesBox.Location = new System.Drawing.Point(12, 12);
+            this.DisplayMessagesBox.Multiline = true;
+            this.DisplayMessagesBox.Name = "DisplayMessagesBox";
+            this.DisplayMessagesBox.ReadOnly = true;
+            this.DisplayMessagesBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.DisplayMessagesBox.Size = new System.Drawing.Size(573, 460);
+            this.DisplayMessagesBox.TabIndex = 4;
+            this.DisplayMessagesBox.TextChanged += new System.EventHandler(this.DisplayMessagesBox_TextChanged);
             // 
-            // tabPage1
+            // ChatBox
             // 
-            this.tabPage1.Location = new System.Drawing.Point(4, 22);
-            this.tabPage1.Name = "tabPage1";
-            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(441, 226);
-            this.tabPage1.TabIndex = 0;
-            this.tabPage1.Text = "tabPage1";
-            this.tabPage1.UseVisualStyleBackColor = true;
-            // 
-            // tabPage2
-            // 
-            this.tabPage2.Location = new System.Drawing.Point(4, 22);
-            this.tabPage2.Name = "tabPage2";
-            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(441, 221);
-            this.tabPage2.TabIndex = 1;
-            this.tabPage2.Text = "tabPage2";
-            this.tabPage2.UseVisualStyleBackColor = true;
+            this.ChatBox.AcceptsReturn = true;
+            this.ChatBox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.ChatBox.Location = new System.Drawing.Point(12, 491);
+            this.ChatBox.Multiline = true;
+            this.ChatBox.Name = "ChatBox";
+            this.ChatBox.Size = new System.Drawing.Size(573, 20);
+            this.ChatBox.TabIndex = 5;
+            this.ChatBox.TextChanged += new System.EventHandler(this.ChatBox_TextChanged);
+            this.ChatBox.KeyDown += new System.Windows.Forms.KeyEventHandler(this.ChatBox_KeyDown);
             // 
             // Form1
             // 
+            this.AccessibleName = "";
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(462, 253);
-            this.Controls.Add(this.tabControl1);
+            this.ClientSize = new System.Drawing.Size(894, 523);
+            this.Controls.Add(this.ChatBox);
+            this.Controls.Add(this.DisplayMessagesBox);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
-            this.Text = "DZ1";
+            this.Text = "Дешевая Версия mIRC";
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).EndInit();
-            this.tabControl1.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -133,9 +128,8 @@
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Label label1;
         private System.IO.FileSystemWatcher fileSystemWatcher1;
-        private System.Windows.Forms.TabControl tabControl1;
-        private System.Windows.Forms.TabPage tabPage1;
-        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TextBox DisplayMessagesBox;
+        private System.Windows.Forms.TextBox ChatBox;
     }
 }
 
--- DZ1/DZ1/Form1.cs
+++ DZ1/DZ1/Form1.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.Remoting.Messaging;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -45,6 +46,28 @@ namespace DZ1
         private void tabPage1_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private void eventLog1_EntryWritten(object sender, System.Diagnostics.EntryWrittenEventArgs e)
+        {
+
+        }
+
+        private void ChatBox_TextChanged(object sender, EventArgs e)
+        {
+            
+            
+        }
+
+        private void DisplayMessagesBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ChatBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Return)
+            DisplayMessagesBox.Text += ChatBox.Text;
         }
     }
 }
