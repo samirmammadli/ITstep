58b5b8bd0ba470a8702269fd04d425352f94158a
samir4ik86 <samir4ik86@gmail.com>
2017-11-25 21:53:57 +0400

    ju

--- CSharp Lesson1/CSharp Lesson1/Program.cs
+++ CSharp Lesson1/CSharp Lesson1/Program.cs
@@ -9,115 +9,32 @@ using System.Threading.Tasks;
 
 namespace CSharp_Lesson1
 {
-    class Time
-    {
-        private int _hours;
-        private int _minutes;
-        private int _seconds;
-        private int _time_in_seconds;
-
-        public Time() : this(0, 0, 0) {}
-
-        public Time(int hour, int minute, int second)
-        {
-            _hours = hour;
-            _minutes = minute;
-            _seconds = second;
-            _time_in_seconds = _hours * 3600 + _minutes * 60 + _seconds;
-        }
-
-        private int ConvertToSeconds(int hours, int minutes, int seconds)
-        {
-            return hours * 3600 + minutes * 60 + seconds;
-             
-        }
-
-        private void ConvertToSeconds()
-        {
-            _time_in_seconds = _hours * 3600 + _minutes * 60 + _seconds;
-
-        }
-
-        private void ConvertToTime()
-        {
-            int sec = _time_in_seconds;
-            _hours = sec / 3600;
-            sec -= _hours * 3600;
-            _minutes = sec / 60;
-            sec -= _minutes * 60;
-            _seconds = sec;
-        }
-
-        private void ConvertToTime(int hours, int minutes, int seconds)
-        {
-            int sec = _time_in_seconds;
-            hours = sec / 3600;
-            sec -= hours * 3600;
-            minutes = sec / 60;
-            sec -= minutes * 60;
-            seconds = sec;
-        }
-
-        private string appendNull(int value)
-        {
-            if (value < 10)
-                return "0" + value.ToString();
-            return value.ToString();
-        }
-
-        static public Time operator - (Time t1, Time t2)
-        {
-            Time t = new Time();
-            t1.ConvertToSeconds();
-            t2.ConvertToSeconds();
-            t._time_in_seconds = t1._time_in_seconds - t2._time_in_seconds;
-            t.ConvertToTime();
-            return t;
-        }
-
-        static public bool operator == (Time t1, Time t2)
-        {
-            if (t1._hours == t2._hours && t1._minutes == t2._minutes && t1._seconds == t2._seconds)
-                return true;
-            else
-                return false;
-        }
-
-        static public bool operator !=(Time obj1, Time obj2)
-        {
-            return !(obj1 == obj2);
-        }
-
-        public override bool Equals(object obj)
-        {
-            return base.Equals(obj);
-        }
-
-        public override int GetHashCode()
-        {
-            return base.GetHashCode();
-        }
-
-        public override string ToString()
-        {
-            return appendNull(_hours) + ":" + appendNull(_minutes) + ":" + appendNull(_seconds);
-        }
-    }
     class Program
     { 
         static void Main(string[] args)
         {
-          
-            Time t = new Time(9,15,7);
-            Time t1 = new Time(16, 24, 18);
 
-            Console.WriteLine(t1);
-            Console.WriteLine(t);
-            Console.WriteLine(t - t1);
+            var t1 = new Time(16, 24, 18);
 
-
-
+            var t = new Time(14,0,14);
             
+
+            DateTime da = new DateTime();
+            da = DateTime.Now;
+            Console.WriteLine(da.TimeOfDay.TotalDays);
+
+            Console.WriteLine("t1 = " + t1);
+            Console.WriteLine("t = " + t);
+
+            Console.WriteLine("t1 > t = " + (t1 > t));
+            Console.WriteLine("t1 < t = " + (t1 < t));
+            Console.WriteLine("t - t1 = " + (t - t1));
+            Console.WriteLine(t);
+
+
+            Console.WriteLine(t - 775231);
+
+            Console.WriteLine(t1 == t);
         }
     }
 }
---
+++ CSharp Lesson1/CSharp Lesson1/Time.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace CSharp_Lesson1
+{
+    class Time
+    {
+        private int _hours;
+        private int _minutes;
+        private int _seconds;
+        private int _timeInSeconds;
+
+        public Time() : this(0, 0, 0) {}
+
+        public Time(int seconds) : this(0, 0, seconds) { }
+
+        public Time(int hour, int minute, int second)
+        {
+
+            if (hour < 0 || hour > 23) throw new ArgumentException("Incorrect value in this case!", nameof(hour));
+            if (minute < 0 || minute > 59) throw new ArgumentException("Incorrect value in this case!", nameof(minute));
+            if (second < 0 || second > 59) throw new ArgumentException("Incorrect value in this case!", nameof(second));
+            Hour = hour;
+            Minute = minute;
+            Second = second;
+            ConvertToSeconds();
+        }
+
+        public int Hour
+        {
+            private set => _hours = value;
+            get => _hours;
+        }
+
+        public int Minute
+        {
+            private set => _minutes = value;
+            get => _minutes;
+        }
+
+        public int Second
+        {
+            private set => _seconds = value;
+            get => _seconds;
+        }
+
+        private void ConvertToSeconds()
+        {
+            _timeInSeconds = _hours * 3600 + _minutes * 60 + _seconds;
+
+        }
+
+        private void ConvertToTime()
+        {
+            var sec = Math.Abs(_timeInSeconds);
+            _hours = sec / 3600;
+            sec -= _hours * 3600;
+            _minutes = sec / 60;
+            sec -= _minutes * 60;
+            _seconds = sec;
+        }
+
+        private static string AppendNull(int value)
+        {
+            if (value < 10)
+                return "0" + value.ToString();
+            return value.ToString();
+        }
+
+        public static Time operator - (Time t1, Time t2)
+        {
+            Time t = new Time();
+            t._timeInSeconds = t1._timeInSeconds - t2._timeInSeconds;
+            t.ConvertToTime();
+            return t;
+        }
+
+        public static Time operator -(Time t1, int sec)
+        {
+            Time t = new Time();
+            t._timeInSeconds = t1._timeInSeconds - sec;
+            t.ConvertToTime();
+            return t;
+        }
+
+        public static bool operator >(Time t1, Time t2)
+        {
+            return t1._timeInSeconds > t2._timeInSeconds;
+        }
+
+        public static bool operator <(Time t1, Time t2)
+        {
+            return !(t1 > t2);
+        }
+
+        public static bool operator == (Time t1, Time t2)
+        {
+            return t1._timeInSeconds == t2._timeInSeconds;
+        }
+
+        public static bool operator !=(Time obj1, Time obj2)
+        {
+            return !(obj1 == obj2);
+        }
+
+        public override bool Equals(object obj) => this == (obj as Time);
+
+        public override int GetHashCode() => this.ToString().GetHashCode();
+
+        public override string ToString()
+        {
+            return (_timeInSeconds < 0 ? "-" : "") + AppendNull(_hours) + ":" + AppendNull(_minutes) + ":" + AppendNull(_seconds);
+        }
+    }
+}
